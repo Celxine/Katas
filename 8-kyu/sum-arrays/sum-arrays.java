35e517a79aaa9b9a29651ef8ac8384ac8e2a5635
Celine Ingabire <ingabireceline54@gmail.com>
2025-11-18 11:15:49 +0200

    Add solution - CodeHub

---
+++ 8-kyu/sum-arrays/sum-arrays.java
@@ -0,0 +1,16 @@
+​
+public class SumArray {
+​
+  public static double sum(double[] numbers) {
+    
+    if(numbers == null || numbers.length ==0){
+      return 0;
+    }
+    double sum=0;
+    for(int i=0; i< numbers.length; i++){
+      sum+=  numbers[i];
+    }
+    return sum;
+}
+  }
+  
