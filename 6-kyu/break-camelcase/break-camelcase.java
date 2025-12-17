7a4ebea049afc68bd3c573362b24621290eaf62f
Celine Ingabire <ingabireceline54@gmail.com>
2025-12-17 19:18:08 +0200

    Update solution - CodeHub

--- 6-kyu/break-camelcase/break-camelcase.java
+++ 6-kyu/break-camelcase/break-camelcase.java
@@ -1,15 +1,18 @@
 class Solution {
   public static String camelCase(String input) {
-  String result= "";
-    
-    for(int i = 0; i < input.length(); i++){
-      char c= input.charAt(i);
-      if( Character.isUpperCase(c)){
-result += " ";
+   
+   String result = "";
+​
+    for (int i = 0; i < input.length(); i++) {
+      char ch = input.charAt(i);
+​
+      if (Character.isUpperCase(ch)) {
+        result += " ";
       }
-      result += c;
+​
+      result += ch;
     }
+​
     return result;
   }
-  
 }
