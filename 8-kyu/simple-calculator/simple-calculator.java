5dfc7a82d642129a4c021895e3eccac2781fe6d0
Celine Ingabire <ingabireceline54@gmail.com>
2026-01-06 10:46:02 +0200

    Add solution - CodeHub

---
+++ 8-kyu/simple-calculator/simple-calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+  public static double calculate(double a, double b, String op) {
+    
+   switch(op){
+       case "+":
+       return a + b;
+       case "-":
+       return a - b;
+       case "*":
+       return a * b;
+       case "/":
+       return a / b;
+       
+       default:
+       throw new IllegalArgumentException("Invalid operation:" + op);
+   } 
+   
+     
+  }
+}
